61a3198ae5cf7a85a44ce08dd8d56478b0640498
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-03-24 21:09:33 +0100

    Actions

---
+++ actions/release-canary/action.js
@@ -0,0 +1,36 @@
+/// @ts-check
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+const core = require('@actions/core');
+
+async function main() {
+  try {
+    const token = core.getInput('npm-token');
+
+    if (!token) {
+      core.info('Skipping... Missing npm-token input');
+      return;
+    }
+
+    try {
+      fs.unlinkSync('out.txt');
+    } catch (e) {}
+
+    fs.writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${token}`, {
+      encoding: 'utf-8',
+    });
+
+    const output = execSync('npm run ci:release:canary > out.txt', {
+      encoding: 'utf-8',
+    });
+
+    const version = output.match(/=\> ([a-z0-9\.\-\+]+)\n/)[1];
+
+    core.setOutput('version', version);
+  } catch (error) {
+    core.setFailed(`Failed to release canary version: ${error.message}`);
+  }
+}
+
+main();
